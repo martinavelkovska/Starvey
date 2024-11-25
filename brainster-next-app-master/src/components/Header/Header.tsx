e21b5d87ed8f786802fad220050995f0e8d55b57
Martina <Martina@DESKTOP-QGG76LM>
2024-11-25 22:59:30 +0100

    animation for logo

--- brainster-next-app-master/src/components/Header/Header.tsx
+++ brainster-next-app-master/src/components/Header/Header.tsx
@@ -1,20 +1,29 @@
 "use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
 import Link from "next/link";
+//import { motion } from "motion/react"
+
 import DarkModeSwitcher from "../DarkModeSwitcher/DarkModeSwitcher";
+
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-999 flex w-full drop-shadow-none bg-black ">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4">
-          <Link className="block flex-shrink-0" href="/dashboard/surveys">
+          <Link className="block flex-shrink-0" href="/dashboard/welcome">
+      
+           <motion.div whileHover={{ scale: 1.2 }} transition={{ duration: 0.3 }}>
             <Image
               width={40}
               height={40}
               src={"/images/logo/star-sparkle.svg"}
               alt="Logo"
             />
+            </motion.div>
+       
+            
           </Link>
         </div>
 
